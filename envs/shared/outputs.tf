e613f03fd29b07f05341927ae8f9f18abbbd04f3
Bryan Pineda <bryan.pinedag@hotmail.com>
2026-09-02 01:06:11 -0600

    fixed outputs to reflect actual module outputs

--- envs/shared/outputs.tf
+++ envs/shared/outputs.tf
@@ -5,12 +5,12 @@ output "acr_id" {
 
 output "acr_name" {
   description = "The name of the Azure Container Registry."
-  value       = module.acr.name
+  value       = module.acr.acr_name
 }
 
 output "acr_login_server" {
   description = "The login server URL of the Azure Container Registry."
-  value       = module.acr.login_server
+  value       = module.acr.acr_login_server
 }
 
 output "github_oidc_client_ids" {
