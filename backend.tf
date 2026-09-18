29ab578846a12d0779bef1ab40bac94dca1a6676
Bryan Pineda <bryan.pinedag@hotmail.com>
2026-09-18 00:35:38 -0600

    added support for multiple environments

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstateharupandi"
     container_name       = "tfstate"
-    key                  = "dev.tfstate"
     use_azuread_auth     = true
   }
 }
